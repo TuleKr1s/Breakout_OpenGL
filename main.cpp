7f0ab2215d7c1157dfd740698b0512abc39a40d6
TuleKr1s <mr.darkwin2018@mail.ru>
2024-01-07 16:48:38 +0300

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <glad/glad.h> // обязательно перед другими
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+
+int main() {
+	
+	return 0;
+}
